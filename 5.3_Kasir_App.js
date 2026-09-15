625df053c367d52b832ec6be0f98159f5a398dce
Ahmad Ngawi <ahmad.ma136new@gmail.com>
2026-09-15 07:53:41 +0700

    add tambahItem()

--- 5.3_Kasir_App.js
+++ 5.3_Kasir_App.js
@@ -20,3 +20,38 @@ const produk = [
     harga: 8000,
   },
 ];
+
+const keranjang = [];
+
+function cariProduk(id) {
+  for (let i = 0; i < produk.length; i++) {
+    if (produk[i].id === id) return produk[i];
+    return null;
+  }
+}
+
+function tambahItem(id, jumlah) {
+  const item = cariProduk(id);
+
+  if (item === null) {
+    console.log("Item tidak ditemukan...");
+    return;
+  }
+
+  for (let i = 0; i < keranjang.length; i++) {
+    if (keranjang[i].id === id) {
+      keranjang[i].jumlah += jumlah;
+      console.log(`Jumlah ${item.nama} ditambah menjadi ${item.jumlah}`);
+      return;
+    }
+  }
+
+  keranjang.push({
+    id: item.id,
+    nama: item.nama,
+    harga: item.harga,
+    jumlah: item.jumlah,
+  });
+
+  console.log(`${item.nama} x${jumlah} ditambahkan`);
+}
