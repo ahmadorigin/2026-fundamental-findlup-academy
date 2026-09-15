7e88d529bc3bef7dac8ed0aa524b074f0b214971
Ahmad Ngawi <ahmad.ma136new@gmail.com>
2026-09-15 07:41:47 +0700

    Commit tugas 5.3_kasir_app

---
+++ 5.3_Kasir_App.js
@@ -0,0 +1,22 @@
+const produk = [
+  {
+    id: 1,
+    nama: "Nasi Goreng",
+    harga: 15000,
+  },
+  {
+    id: 2,
+    nama: "Mie Ayam",
+    harga: 12000,
+  },
+  {
+    id: 3,
+    nama: "Es Teh",
+    harga: 5000,
+  },
+  {
+    id: 4,
+    nama: "Jus Jeruk",
+    harga: 8000,
+  },
+];
