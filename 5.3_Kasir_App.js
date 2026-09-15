aefb40ccae60f4c7fa233a328501dee11aab0037
Ahmad Ngawi <ahmad.ma136new@gmail.com>
2026-09-15 11:27:48 +0700

    end Tasks

--- 5.3_Kasir_App.js
+++ 5.3_Kasir_App.js
@@ -1,37 +1,24 @@
 const produk = [
-  {
-    id: 1,
-    nama: "Nasi Goreng",
-    harga: 15000,
-  },
-  {
-    id: 2,
-    nama: "Mie Ayam",
-    harga: 12000,
-  },
-  {
-    id: 3,
-    nama: "Es Teh",
-    harga: 5000,
-  },
-  {
-    id: 4,
-    nama: "Jus Jeruk",
-    harga: 8000,
-  },
+  { id: 1, nama: "Nasi Goreng", harga: 15000 },
+  { id: 2, nama: "Mie Ayam", harga: 12000 },
+  { id: 3, nama: "Es Teh", harga: 5000 },
+  { id: 4, nama: "Jus Jeruk", harga: 8000 },
+  { id: 5, nama: "Pisang Goreng", harga: 7000 },
 ];
 
 const keranjang = [];
 
 function cariProduk(id) {
   for (let i = 0; i < produk.length; i++) {
-    if (produk[i].id === id) return produk[i];
-    return null;
+    if (produk[i].id === id) {
+      return produk[i];
+    }
   }
+  return null;
 }
 
 function tambahItem(id, jumlah) {
-  const item = cariProduk(id);
+  const item = cariProduk(Number(id));
 
   if (item === null) {
     console.log("Item tidak ditemukan...");
@@ -41,7 +28,9 @@ function tambahItem(id, jumlah) {
   for (let i = 0; i < keranjang.length; i++) {
     if (keranjang[i].id === id) {
       keranjang[i].jumlah += jumlah;
-      console.log(`Jumlah ${item.nama} ditambah menjadi ${item.jumlah}`);
+      console.log(
+        `Jumlah ${item.nama} ditambah menjadi ${keranjang[i].jumlah}`,
+      );
       return;
     }
   }
@@ -50,7 +39,7 @@ function tambahItem(id, jumlah) {
     id: item.id,
     nama: item.nama,
     harga: item.harga,
-    jumlah: item.jumlah,
+    jumlah: jumlah,
   });
 
   console.log(`${item.nama} x${jumlah} ditambahkan`);
@@ -70,3 +59,31 @@ function hitungDiskon(total) {
   if (total >= 30000) return total * 0.05;
   return 0;
 }
+
+function tampilkanStruk() {
+  console.log("\n===== Struk Belanja Mu =====");
+
+  for (let i = 0; i < keranjang.length; i++) {
+    const item = keranjang[i];
+    const subTotal = item.harga * item.jumlah;
+    console.log(`${item.nama} x${item.jumlah} = Rp${subTotal}`);
+  }
+
+  const total = hitungSubTotal();
+  const diskon = hitungDiskon(total);
+  const bayar = total - diskon;
+
+  console.log("--------------------");
+  console.log(`Total: ${total}`);
+  console.log(`Diskon: ${diskon}`);
+  console.log(`Bayar: ${bayar}`);
+  console.log("====================");
+}
+
+tambahItem(2, 2);
+tambahItem(2, 1);
+tambahItem(5, 3);
+
+console.log("");
+
+tampilkanStruk();
